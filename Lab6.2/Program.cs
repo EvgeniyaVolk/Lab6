5ed150418c2350a8f0ab6a7dd49dc678cb301533
Женя <@GENYASPIN>
2021-11-28 20:36:20 +0300

    Добавьте файлы проекта.

---
+++ Lab6.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string start = Console.ReadLine();
+            start = start.Replace(" ", "");
+            char[] array = start.ToCharArray();
+            Array.Reverse(array);
+            string reverse = new string(array);
+
+            if (start == reverse)
+            {
+                Console.WriteLine("Данная надпись является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Данная надпись не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string start = Console.ReadLine();
+            string[] array = start.Split();
+            int n = 1;
+            int t = 1;
+                for (int j = 0; j < array.Count(); j++)
+                {
+                    n = array[j].Length;
+                    if (n>t)
+                    {
+                        t = n;
+                    }
+                Console.WriteLine("Длина {0} слова = {1}",j, n);
+
+                 }
+
+            Console.WriteLine("Максимальная длина слова = {0} букв", t);
+            Console.ReadKey();
+
+        }
+    }
+}
